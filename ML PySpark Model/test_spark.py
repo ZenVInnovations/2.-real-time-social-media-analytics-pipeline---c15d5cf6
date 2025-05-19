84d3dd8966c03ae4cd08d75b5fffdf73337413f0
KMateenKhan <120782951+KMateenKhan@users.noreply.github.com>
2025-05-19 22:01:57 +0530

    Update test_spark.py

--- ML PySpark Model/test_spark.py
+++ ML PySpark Model/test_spark.py
@@ -1,8 +1,6 @@
 from pyspark.sql import SparkSession
-
 print("🟢 Starting Spark session...")
 spark = SparkSession.builder.appName("TestSpark").getOrCreate()
 print("✅ Spark session started successfully.")
-
 df = spark.createDataFrame([(1, "hello"), (2, "world")], ["id", "text"])
 df.show()
